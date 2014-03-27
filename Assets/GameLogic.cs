376867f7bbd4ccb931d0b2ff3db50dbdf30beff8
Lukas Elmer <lukas.elmer@gmail.com>
2014-03-27 16:10:34 +0100

    game time, gui

---
+++ Assets/GameLogic.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLogic : MonoBehaviour
+{
+	public float PlayTime{ get; private set; }
+
+	void Update ()
+	{
+		PlayTime += Time.deltaTime;
+	}
+}
---
+++ Assets/ScoreBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreBehaviour : MonoBehaviour {
+
+	public GameLogic gameLogic;
+
+	void Update () {
+		this.guiText.text = "Play Time: " + gameLogic.PlayTime;
+	}
+}
