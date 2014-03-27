36ccd32ca243ab71d96a9bbce830d7656ace172b
Lukas Elmer <lukas.elmer@gmail.com>
2014-03-27 15:16:20 +0100

    Movable camera, ground, sky

---
+++ Assets/Camera/CameraBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviour : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		float xAxisValue = Input.GetAxis("Horizontal");
+		float yAxisValue = Input.GetAxis("Vertical");
+		if(gameObject != null)
+		{
+			gameObject.transform.Translate(new Vector3(xAxisValue, 0, yAxisValue));
+		}
+
+	}
+}
