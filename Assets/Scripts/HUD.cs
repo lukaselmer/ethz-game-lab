d2af632fdcd8d68fc250f08783e4b7d371f8bcf6
Ivo Nussbaumer <ivo.nussbaumer@gmail.com>
2014-04-01 14:23:09 +0200

    HUD / InputHandler Refactoring

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUD : MonoBehaviour
+{	
+	public GameLogic gameLogic;
+	public GUIText guiPlayTime;
+
+	public bool PlacementMode { get; set; }
+
+	void Update ()
+	{
+		guiPlayTime.text = string.Format ("Enemies killed: {0}, Enemies survived: {1}, Play Time: {2:F0}", 
+		               gameLogic.EnemiesKilled, gameLogic.EnemiesSurvived, gameLogic.PlayTime);
+	}
+	
+	void OnGUI ()
+	{
+		if (GUI.Button (new Rect (10, Screen.height - 110, 100, 100), PlacementMode ? "Placing..." : "Tower")) {
+			PlacementMode = true;
+		}
+	}
+}
---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputHandler : MonoBehaviour {
+
+	public HUD hud;
+	public TowerPlacement towerPlacement;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton (0)) {	
+			if (hud.PlacementMode) {
+				if (towerPlacement.placeTower(Input.mousePosition)) {
+					hud.PlacementMode = false;
+				}
+			}
+		}
+	
+	}
+}
