3907126655f63c8c3dc1c5277e2aeb14f21cc23c
Lukas Elmer <lukas.elmer@gmail.com>
2014-04-01 02:53:21 +0200

    special start / stop terrains

--- Assets/Scripts/PathPainter.cs
+++ Assets/Scripts/PathPainter.cs
@@ -1,10 +1,8 @@
 using System;
 using UnityEngine;
 
-namespace Game
-{
-	public class PathPainter
-	{
+namespace Game {
+	public class PathPainter {
 		private Checkpoint[] Checkpoints { get; set; }
 
 		TerrainData terrainData;
@@ -12,8 +10,7 @@ namespace Game
 		float widthHeight;
 		const int pathSize = 40;
 		
-		public PathPainter (Checkpoint[] checkpoints, Terrain terrain)
-		{
+		public PathPainter (Checkpoint[] checkpoints, Terrain terrain) {
 			this.Checkpoints = checkpoints;
 
 			terrainData = terrain.terrainData;
@@ -24,46 +21,52 @@ namespace Game
 				throw new Exception ("Invalid terrain configuration!");
 		}
 
-		private float[,,] CreateTerrainMap ()
-		{
-			var map = new float[terrainData.alphamapWidth, terrainData.alphamapHeight, 2];
+		private float[,,] CreateTerrainMap () {
+			var map = new float[terrainData.alphamapWidth, terrainData.alphamapHeight, terrainData.alphamapLayers];
+			//var map = terrainData.GetAlphamaps (0, 0, terrainData.alphamapWidth, terrainData.alphamapHeight);
 			for (var x = 0; x < terrainData.alphamapWidth; ++x) {
 				for (var y = 0; y < terrainData.alphamapHeight; ++y) {
 					map [x, y, 0] = 1f;
 					map [x, y, 1] = 0f;
+					map [x, y, 2] = 0f;
 				}
 			}
 			return map;
 		}
 
-		private void PaintCheckpoint (float[,,] map, Vector2 pos)
-		{
+		private void PaintCheckpoint (float[,,] map, Vector2 pos) {
+			PaintPoint (map, pos, 1, pathSize / 2);
+		}
+		
+		private void PaintPoint (float[,,] map, Vector2 pos, int index, int size) {
 			var cx = (pos.x + widthHeight / 2) / widthHeight * map.GetLength (0);
 			var cy = (pos.y + widthHeight / 2) / widthHeight * map.GetLength (1);
-
-			DrawPoint (map, (int)cx, (int)cy);
+			
+			DrawPoint (map, (int)cx, (int)cy, index, size);
 		}
 
-		private void DrawPoint (float[,,] map, int cx, int cy)
-		{
-			var h = pathSize / 2;
-			float maxDist = (float) Math.Sqrt(h*h*2);
+		private void DrawPoint (float[,,] map, int cx, int cy, int index, int size) {
+			var h = size;
+			float maxDist = (float) h * (float)Math.Sqrt (2);
 
 			for (int x = cx - h; x < cx + h; ++x) {
 				for (int y = cy - h; y < cy + h; ++y) {
 					float dx = Math.Abs (x - cx);
 					float dy = Math.Abs (y - cy);
-					float distToCenter = (float) Math.Sqrt(dx * dx + dy * dy);
+					float distToCenter = (float)Math.Sqrt (dx * dx + dy * dy);
 					float intensity = 1.0f - distToCenter / maxDist;
 
-					map [y, x, 0] = 1f;
-					map [y, x, 1] = Math.Max (intensity, map [y, x, 1]);
+					if (index == 2) {
+						map [y, x, 0] = 1f-intensity;
+					} else {
+						map [y, x, 0] = 1f;
+					}
+					map [y, x, index] = Math.Max (intensity, map [y, x, 1]);
 				}
 			}
 		}
 		
-		public void PaintPath ()
-		{
+		public void PaintPath () {
 			var map = CreateTerrainMap ();
 			var xmin = terrainData.alphamapWidth / 2;
 			var ymin = terrainData.alphamapHeight / 2;
@@ -71,6 +74,11 @@ namespace Game
 			foreach (var checkpoint in Checkpoints) {
 				PaintCheckpoint (map, new Vector2 (checkpoint.Position.x, checkpoint.Position.z));
 			}
+
+			var c1 = Checkpoints [0].Position;
+			PaintPoint (map, new Vector2 (c1.x, c1.z), 2, 50);
+			var c2 = Checkpoints [Checkpoints.Length - 1].Position;
+			PaintPoint (map, new Vector2 (c2.x, c2.z), 2, 30);
 			
 			terrainData.SetAlphamaps (0, 0, map);
 		}
