6fe375a7780a9230e6953a4719181f4c022051bb
Lukas Elmer <lukas.elmer@gmail.com>
2014-04-14 14:55:50 +0200

    refactoring

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -15,13 +15,13 @@ public class GameLogic : MonoBehaviour {
 
 	// Checkpoints
 	public Checkpoint[] checkpoints;
-	private Checkpoint[] interpolatedCheckpoints;
 
-	public Checkpoint EndCheckpoint { get { return interpolatedCheckpoints [interpolatedCheckpoints.Length - 1]; } }
-	public Checkpoint StartCheckpoint { get { return interpolatedCheckpoints [0]; } }
+	public Checkpoint EndCheckpoint { get { return maze.EndCheckpoint; } }
+	public Checkpoint StartCheckpoint { get { return maze.StartCheckpoint; } }
 
 	private WaveManager waveManager;
 
+	private Maze maze;
 	
 	public static GameLogic Instance {
 		get {
@@ -30,8 +30,8 @@ public class GameLogic : MonoBehaviour {
 	}
 
 	void Start () {
-		interpolatedCheckpoints = new Maze(gameObject).InterpolateCheckpoints (checkpoints, 4);
-		new PathPainter (interpolatedCheckpoints, Terrain.activeTerrain).PaintPath ();
+		maze = new Maze(gameObject, checkpoints);
+		new PathPainter (maze.InterpolateCheckpoints (), Terrain.activeTerrain).PaintPath ();
 		
 		waveManager = new WaveManager(this, enemyPrefab, StartCheckpoint, enemyParent);
 		waveManager.StartWaves();
@@ -43,27 +43,12 @@ public class GameLogic : MonoBehaviour {
 	}
 
 	private int IndexOfCheckpoint (Checkpoint checkpoint) {
-		if (interpolatedCheckpoints == null)
-			return 0;
+		return maze.IndexOfCheckpoint(checkpoint);
 
-		for (int i = 0; i < interpolatedCheckpoints.Length; ++i) {
-			if (interpolatedCheckpoints [i] == checkpoint) {
-				return i;
-			}
-		}
-		return -1;
 	}
 
 	public Checkpoint NextCheckpoint (Checkpoint currentCheckpoint) {
-		int index = IndexOfCheckpoint (currentCheckpoint);
-
-		if (index == -1)
-			return interpolatedCheckpoints [0];
-		else if (index < interpolatedCheckpoints.Length - 1)
-			return interpolatedCheckpoints [index + 1];
-
-		// This should never happen!
-		return EndCheckpoint;
+		return maze.NextCheckpoint(currentCheckpoint);
 	}
 
 	public void Finished (Enemy enemy) {
--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -3,13 +3,53 @@ using UnityEngine;
 
 namespace Game {
 	public class Maze {
-		GameObject gameObject;
+		private GameObject gameObject;
+		private Checkpoint[] checkpoints;
+		private Checkpoint[] interpolatedCheckpoints;
 
-		public Maze (GameObject gameObject) {
-			this.gameObject = gameObject;
+		public Checkpoint EndCheckpoint {
+			get{ return checkpoints [checkpoints.Length - 1];}
 		}
-		
-		public Checkpoint[] InterpolateCheckpoints (Checkpoint[] checkpoints, int limit) {
+
+		public Checkpoint StartCheckpoint {
+			get{ return checkpoints [0];}
+		}
+
+		public Maze (GameObject gameObject, Checkpoint[] checkpoints) {
+			this.gameObject = gameObject;
+			this.checkpoints = checkpoints;
+			this.interpolatedCheckpoints = CalculateInterpolateCheckpoints (checkpoints, 4);
+		}
+
+		public Checkpoint[] InterpolateCheckpoints () {
+			return interpolatedCheckpoints;
+		}
+
+		public int IndexOfCheckpoint (Checkpoint checkpoint) {
+			if (interpolatedCheckpoints == null)
+				return 0;
+			
+			for (int i = 0; i < interpolatedCheckpoints.Length; ++i) {
+				if (interpolatedCheckpoints [i] == checkpoint) {
+					return i;
+				}
+			}
+			return -1;
+		}
+
+		public Checkpoint NextCheckpoint (Checkpoint currentCheckpoint) {
+			int index = IndexOfCheckpoint (currentCheckpoint);
+			
+			if (index == -1)
+				return interpolatedCheckpoints [0];
+			else if (index < interpolatedCheckpoints.Length - 1)
+				return interpolatedCheckpoints [index + 1];
+			
+			// This should never happen!
+			return EndCheckpoint;
+		}
+
+		public Checkpoint[] CalculateInterpolateCheckpoints (Checkpoint[] checkpoints, int limit) {
 			if (limit == 0)
 				return checkpoints;
 			
@@ -23,7 +63,7 @@ namespace Game {
 				newCheckpoints [i * 2 - 1] = Interpolate (newCheckpoints [i * 2 - 2], newCheckpoints [i * 2]);
 			}
 			
-			return InterpolateCheckpoints (newCheckpoints, limit - 1);
+			return CalculateInterpolateCheckpoints (newCheckpoints, limit - 1);
 		}
 		
 		Checkpoint Interpolate (Checkpoint c1, Checkpoint c2) {
