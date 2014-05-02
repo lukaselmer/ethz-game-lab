64069908ba3a610e51e122f953e3bead8fff917c
Ivo Nussbaumer <ivo.nussbaumer@gmail.com>
2014-05-02 18:30:55 +0200

    Level Selection

---
+++ Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelect : MonoBehaviour {
+
+	public LayerMask ClickableLayer;
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Handle left click
+		if (Input.GetMouseButton (0)) {	
+
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit, 1000, ClickableLayer)) {
+				
+				var selectedGameObject = hit.collider.gameObject.transform;
+				var level_name = selectedGameObject.name;
+				
+				Application.LoadLevel(level_name);
+			}
+		}
+	}
+}
