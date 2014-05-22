5080b5873f846c300c3b41e7bce04dfb73ad8639
Lukas Elmer <lukas.elmer@gmail.com>
2014-05-22 16:47:06 +0200

    hide locked level buttons

---
+++ Assets/LevelClickable.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelClickable : MonoBehaviour {
+
+	void Start () {
+		if (!LevelSelection.CanPlay(name)){
+			this.renderer.enabled = false;
+		}
+	}
+
+	void AfterUpdate () {
+	
+	}
+}
