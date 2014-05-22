baa58e6b62095c10c213c27a34811f2dfdbb75b6
Lukas Elmer <lukas.elmer@gmail.com>
2014-05-22 17:20:15 +0200

    auto rotate, lock unlocked levels

---
+++ Assets/Scripts/AutoRotate.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoRotate : MonoBehaviour {
+
+	void Update () {
+		transform.RotateAround(transform.position, Vector3.up, 100 * Time.deltaTime);
+	}
+}
